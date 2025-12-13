1f02ac6b3eeaf486b5ba00bfc4f4462a5d12205a
cbgbar <vanntth2307003@fpt.edu.vn>
2025-12-13 14:04:19 +0700

    update

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 const mysql = require('mysql2/promise');
 const { v4: uuidv4 } = require('uuid'); // Import uuid
 
-// Chỉ giữ lại 1 connection string sql heroku 2
-const db_conn = process.env.DATABASE_URL_1 || 'mysql://bzihyhgnacffc6js:ef46fe0sl8c8zem8@uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xclxsvhwwz9xuwz8';
+// Chỉ giữ lại 1 connection string sql heroku dbwedding 3
+const db_conn = process.env.DATABASE_URL_1 || 'mysql://d7xh4dxvhoykba91:s26dru7o4t5slog7@l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/y2n7m8fe32s9fhgf';
 
 // Tạo 1 pool duy nhất
 const pool = mysql.createPool(db_conn);
